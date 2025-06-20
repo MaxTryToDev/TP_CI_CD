9c6486b2177d4610f8a26f18c819324ba29d5340
Maxime BRIFFAUD <maximebriffaud@hotmail.fr>
2025-06-20 17:18:38 +0200

    Test

--- tests/example.e2e.spec.js
+++ tests/example.e2e.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
 test('page d\'accueil affiche le bon titre', async ({ page }) => {
   await page.goto('https://example.com');
