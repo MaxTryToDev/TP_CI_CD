9fe23e92670ffc1125148323ba8ae0012b025e2f
Maxime BRIFFAUD <maximebriffaud@hotmail.fr>
2025-06-20 17:04:28 +0200

    Ajout du build

---
+++ tests/example.e2e.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('page d\'accueil affiche le bon titre', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
