9e60934ff8702345bc9f44d89b167e6873755d37
Maxime BRIFFAUD <maximebriffaud@hotmail.fr>
2025-06-20 17:07:42 +0200

    Ajout config eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      "no-unused-vars": "warn",
+    },
+  },
+];
